1709b6b059a9f1924fb55a7a889d9a76506a6540
Ryo Fujimura <ryo176999@gmail.com>
2023-07-20 15:10:32 -0700

    Lights Out 4.3.2
    4.3.1 not yet

--- LightsOut/app/src/main/java/com/zybooks/lightsout/MainActivity.kt
+++ LightsOut/app/src/main/java/com/zybooks/lightsout/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         lightOnColor = ContextCompat.getColor(this, R.color.yellow)
-        lightOffColor = ContextCompat.getColor(this, R.color.grey)
+        lightOffColor = ContextCompat.getColor(this, R.color.black)
 
         game = LightsOutGame()
 
