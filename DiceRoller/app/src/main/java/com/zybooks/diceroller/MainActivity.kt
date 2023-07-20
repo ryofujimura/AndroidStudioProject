a14970780ddd6b151daa7701744097b1d1758db9
Ryo Fujimura <ryo176999@gmail.com>
2023-07-20 11:41:43 -0700

    Dice 5.3

--- DiceRoller/app/src/main/java/com/zybooks/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/zybooks/diceroller/MainActivity.kt
@@ -1,11 +1,135 @@
 package com.zybooks.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity() {
+const val MAX_DICE = 3
+
+class MainActivity : AppCompatActivity(),RollLengthDialogFragment.OnRollLengthSelectedListener {
+
+    private var numVisibleDice = MAX_DICE
+    private lateinit var diceList: MutableList<Dice>
+    private lateinit var diceImageViewList: MutableList<ImageView>
+    private lateinit var optionsMenu: Menu
+    private var timer: CountDownTimer? = null
+    private var timerLength = 2000L
+    private var selectedDie = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create list of Dice
+        diceList = mutableListOf()
+        for (i in 0 until MAX_DICE) {
+            diceList.add(Dice(i + 1))
+        }
+
+        // Create list of ImageViews
+        diceImageViewList = mutableListOf(
+            findViewById(R.id.dice1), findViewById(R.id.dice2), findViewById(R.id.dice3))
+
+        showDice()
+        // Register context menus for all dice and tag each die
+        for (i in 0 until diceImageViewList.size) {
+            registerForContextMenu(diceImageViewList[i])
+            diceImageViewList[i].tag = i
+        }
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.appbar_menu, menu)
+        optionsMenu = menu!!
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun showDice() {
+
+        // Show visible dice
+        for (i in 0 until numVisibleDice) {
+            val diceDrawable = ContextCompat.getDrawable(this, diceList[i].imageId)
+            diceImageViewList[i].setImageDrawable(diceDrawable)
+            diceImageViewList[i].contentDescription = diceList[i].imageId.toString()
+        }
+    }
+
+    override fun onRollLengthClick(which: Int) {
+        // Convert to milliseconds
+        timerLength = 1000L * (which + 1)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        // Determine which menu option was chosen
+        return when (item.itemId) {
+            R.id.action_one -> {
+                changeDiceVisibility(1)
+                showDice()
+                true
+            }
+            R.id.action_two -> {
+                changeDiceVisibility(2)
+                showDice()
+                true
+            }
+            R.id.action_three -> {
+                changeDiceVisibility(3)
+                showDice()
+                true
+            }
+            R.id.action_stop -> {
+                timer?.cancel()
+                item.isVisible = false
+                true
+            }
+            R.id.action_roll -> {
+                rollDice()
+                true
+            }
+            R.id.action_roll_length -> {
+                val dialog = RollLengthDialogFragment()
+                dialog.show(supportFragmentManager, "rollLengthDialog")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+    private fun rollDice() {
+        optionsMenu.findItem(R.id.action_stop).isVisible = true
+        timer?.cancel()
+
+        // Start a timer that periodically changes each visible dice
+        timer = object : CountDownTimer(timerLength, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                for (i in 0 until numVisibleDice) {
+                    diceList[i].roll()
+                }
+                showDice()
+            }
+
+            override fun onFinish() {
+                optionsMenu.findItem(R.id.action_stop).isVisible = false
+            }
+        }.start()
+    }
+
+    private fun changeDiceVisibility(numVisible: Int) {
+        numVisibleDice = numVisible
+
+        // Make dice visible
+        for (i in 0 until numVisible) {
+            diceImageViewList[i].visibility = View.VISIBLE
+        }
+
+        // Hide remaining dice
+        for (i in numVisible until MAX_DICE) {
+            diceImageViewList[i].visibility = View.GONE
+        }
     }
 }
