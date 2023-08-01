98caea77e287aea5194ecb6f5be6f024848abd01
Ryo Fujimura <ryo176999@gmail.com>
2023-08-01 11:49:21 -0700

    8.1.1.1

--- Fibonacci/app/src/main/java/com/zybooks/fibonacci/MainActivity.kt
+++ Fibonacci/app/src/main/java/com/zybooks/fibonacci/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.zybooks.fibonacci
 
-import android.media.tv.TvContract.Programs
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ProgressBar
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.NumberFormat
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
     private lateinit var numberEditText: EditText
     private lateinit var resultTextView: TextView
-    private lateinit var NumberFormat: NumberFormat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,19 +20,18 @@ class MainActivity : AppCompatActivity() {
         progressBar = findViewById(R.id.progressBar)
         numberEditText = findViewById(R.id.numberEditText)
         resultTextView = findViewById(R.id.resultTextView)
-
     }
 
     private fun fibonacci(n: Long): Long {
         return if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
     }
 
-    fun fibonacciClick(view: View) {
+    fun fibonacciClickOne(view: View) {
         // Display progress bar
         progressBar.visibility = View.VISIBLE
 
         // Find the nth Fibonacci number using the given number
-        val num = numberEditText.text.toString().toLong()
+        val num = numberEditText.text.toString().toLongOrNull()
         val fibNumber = fibonacci(num)
 
         // Show the result with commas in the right place
@@ -41,4 +42,33 @@ class MainActivity : AppCompatActivity() {
         progressBar.visibility = View.INVISIBLE
     }
 
+    fun fibonacciClickTwo(view: View) {
+
+        // Display progress bar
+        progressBar.visibility = View.VISIBLE
+
+        // Get number from UI
+        val num = numberEditText.text.toString().toLongOrNull()
+
+        // Clear previous result
+        resultTextView.text = ""
+
+        // Create a background thread
+        val thread = Thread {
+
+            // Find the Fibonacci number
+            val fibNumber = fibonacci(num)
+
+            // UI should only be updated by main thread
+            runOnUiThread {
+                resultTextView.text = "Result:" +
+                        NumberFormat.getNumberInstance(Locale.US).format(fibNumber)
+
+                // Hide progress bar
+                progressBar.visibility = View.INVISIBLE
+            }
+        }
+
+        thread.start()
+    }
 }
