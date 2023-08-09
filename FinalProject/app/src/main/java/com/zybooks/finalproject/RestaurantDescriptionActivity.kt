d535cf33ea3e47eb67e3c7a2633fbc54b1fa354f
Ryo Fujimura <ryo176999@gmail.com>
2023-08-08 17:09:05 -0700

    description name working

--- FinalProject/app/src/main/java/com/zybooks/finalproject/RestaurantDescriptionActivity.kt
+++ FinalProject/app/src/main/java/com/zybooks/finalproject/RestaurantDescriptionActivity.kt
@@ -18,20 +18,13 @@ class RestaurantDescriptionActivity : AppCompatActivity() {
 
         val restaurantNameTextView = findViewById<TextView>(R.id.restaurantNameTextView)
         val addressTextView = findViewById<TextView>(R.id.addressTextView)
-        val descriptionTextView = findViewById<TextView>(R.id.descriptionTextView)
         val imageView1 = findViewById<ImageView>(R.id.imageView1)
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
         val imageView3 = findViewById<ImageView>(R.id.imageView3)
 
         restaurantNameTextView.text = selectedRestaurant.name
         addressTextView.text = selectedRestaurant.address
-        descriptionTextView.text = selectedRestaurant.description
 
-        // Load images using Picasso library (replace with your image URLs)
-        if (selectedRestaurant.images.isNotEmpty()) {
-            Picasso.get().load(selectedRestaurant.images[0]).into(imageView1)
-            Picasso.get().load(selectedRestaurant.images[1]).into(imageView2)
-            Picasso.get().load(selectedRestaurant.images[2]).into(imageView3)
-        }
+
     }
 }
