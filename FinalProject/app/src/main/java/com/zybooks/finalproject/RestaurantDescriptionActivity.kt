349a241d532d43b5f51e224c12f640bc17c97a3b
Ryo Fujimura <ryo176999@gmail.com>
2023-08-08 17:35:12 -0700

    description working

--- FinalProject/app/src/main/java/com/zybooks/finalproject/RestaurantDescriptionActivity.kt
+++ FinalProject/app/src/main/java/com/zybooks/finalproject/RestaurantDescriptionActivity.kt
@@ -1,32 +1,55 @@
 package com.zybooks.finalproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Response
+import java.io.IOException
 
 class RestaurantDescriptionActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_description)
 
-        val selectedRestaurantPosition = intent.getIntExtra("selectedRestaurantPosition", -1)
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body?.string()
+                val restaurants = gson.fromJson(responseBody, MainActivity.YelpApiResponse::class.java)
 
-        if (selectedRestaurantPosition != -1) {
-            val selectedRestaurant = MainActivity.restaurants.businesses[selectedRestaurantPosition]
+                runOnUiThread {
+                    val adapter = object : ArrayAdapter<MainActivity.Business>(this@RestaurantDescriptionActivity, R.layout.list_item_restaurant, restaurants.businesses) {
+                        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+                            val itemView = layoutInflater.inflate(R.layout.list_item_restaurant, parent, false)
+                            val restaurant = getItem(position)
 
-            val restaurantImageView1 = findViewById<ImageView>(R.id.restaurantImageView1)
-            val restaurantImageView2 = findViewById<ImageView>(R.id.restaurantImageView2)
-            val restaurantImageView3 = findViewById<ImageView>(R.id.restaurantImageView3)
+                            val restaurantNameTextView = itemView.findViewById<TextView>(R.id.restaurantNameTextView)
+                            val restaurantImageView = itemView.findViewById<ImageView>(R.id.restaurantImageView)
 
-            val imageUrls = selectedRestaurant.image_url // Assuming 'photos' is a list of image URLs
+                            if (restaurant != null) {
+                                restaurantNameTextView.text = restaurant.name
+                            }
+                            if (restaurant != null) {
+                                Glide.with(this@RestaurantDescriptionActivity).load(restaurant.image_url).into(restaurantImageView)
+                            }
 
-            if (imageUrls.isNotEmpty()) {
-                Glide.with(this).load(imageUrls[0]).into(restaurantImageView1)
-                Glide.with(this).load(imageUrls[1]).into(restaurantImageView2)
-                Glide.with(this).load(imageUrls[2]).into(restaurantImageView3)
+                            return itemView
+                        }
+                    }
+                    restaurantImageView1.adapter = adapter
+                }
             }
-        }
+
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+        })
     }
 }
